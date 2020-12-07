cddb6465396759c022c5ad1a51d051efaab0f444
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2020-12-08 02:10:53 +0600

    Create 1136.cpp

---
+++ 1136.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<cstring>
+//#define Mfc_Tanzim
+#define   ll         long long
+#define   ull        unsigned long long
+#define   pb         push_back
+#define   ff         first
+#define   ss         second
+#define   all(v)     (v.begin(), v.end())
+#define   rall(v)    (v.rbegin(), v.rend())
+#define   pi         acos(-1.0)
+#define   FastRead   ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define   bug(a)     cerr << #a << " : " << a << endl
+using namespace std;
+const ll mod = 1e9+7;
+const ll mx = 2e6+10;
+
+int posx[] = {1,-1, 0, 0};
+int posy[] = {0, 0, 1,-1};
+
+int cnt(int n)
+{
+    int ret = 0;
+    if(n==0)return ret;
+
+    ret+=(n/3)*2;
+    ret+=(n%3==2);
+
+    return ret;
+}
+
+int main()
+{
+    FastRead
+
+#ifdef Mfc_Tanzim
+    freopen("input.txt","r", stdin);
+    // freopen("output.txt","w", stdout);
+#endif /// Mfc_Tanzim
+
+    ll t=1, n, k, m, a, b, cs=0, d;
+    scanf("%lld", &t);
+
+    while(t--){
+            scanf("%lld %lld", &a, &b);
+            printf("Case %lld: %d\n", ++cs, cnt(b)-cnt(a-1));
+    }
+    return 0;
+}
