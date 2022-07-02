dcfb71d72511a99a47fbde648bb99d90145a7ef9
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2022-07-03 03:26:29 +0600

    Update 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -1,85 +1,64 @@
 #include<bits/stdc++.h>
-#include<cstring>
-//#define Mfc_Tanzim
-#define  ll        long long
-#define  ull       unsigned long long
-#define  pb        push_back
-#define  ff        first
-#define  ss        second
-#define  all(v)    (v.begin(), v.end())
-#define  pi        acos(-1.0)
-#define  FastRead  ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
-#define  bug(a)    cerr << #a << " : " << a << endl
 using namespace std;
-const ll mx = 1000;
-const ll maxx = 1e17;
-ll posx[] = {1,-1, 0, 0};
-ll posy[] = {0, 0, 1,-1};
+#define bug(a) cerr<<#a<<" : "<<a<<endl
+using ll=long long;
 
-vector<pair<ll, ll> >graph[mx];
-ll dis[mx];
-priority_queue<pair<ll, ll> >pq;
-
-void Dijkstra(ll u)
-{
-    pq.push({0, u});
-    dis[u] = 0;
-    while(!pq.empty()){
-            ll x = pq.top().second;
-            ll y = pq.top().first;
-            pq.pop();
-            for(auto i: graph[x]){
-                      ll cost = max(i.second, y);
-                      ll current_node = i.first;
-                      if(cost<dis[current_node]){
-                                   dis[current_node] = cost;
-                                   pq.push({dis[current_node], current_node});
-                      }
-            }
+struct data{
+    int u, v, cost;
+    data(){}
+    data(int _u, int _v, int _cost){
+          u = _u;
+          v = _v;
+          cost = _cost;
     }
-}
+};
+vector<data>graph;
+int dis[551];
 
-void init(ll n)
+void Bellman_Ford(int src, int nodes)
 {
-    for(ll i=0; i<n; i++){
-            dis[i] = maxx;
-            if(graph[i].size())
-                    graph[i].clear();
+    for(int i=0; i<nodes; i++)dis[i]=INT_MAX;
+    dis[src]=0;
+    for(int j=1; j<nodes; j++){
+          for(int i=0; i<graph.size(); i++){
+                int cur = max(dis[graph[i].u], graph[i].cost);
+                if(cur<dis[graph[i].v]){
+                      dis[graph[i].v] = cur;
+                }
+                cur = max(dis[graph[i].v], graph[i].cost);
+                if(cur<dis[graph[i].u]){
+                      dis[graph[i].u] = cur;
+                }
+           }
     }
+    graph.clear();
 }
 
 int main()
 {
-    //FastRead
+    int t=1, n, m, k;
+    cin >> t;
 
-#ifdef Mfc_Tanzim
-    freopen("input.txt","r", stdin);
-    // freopen("output.txt","w", stdout);
-#endif // Mfc_Tanzim
+    int cs=0;
+    while(t--){
+            scanf("%d %d", &n, &m);
+            while(m--){
+                   int u, v, w;
+                   scanf("%d %d %d", &u, &v, &w);
+                   graph.push_back(data(u, v, w));
+            }
+            int src;
+            scanf("%d", &src);
+            Bellman_Ford(src, n);
 
-    ll n, m, u, v, w, src, tar, t;
-
-    scanf("%lld", &t);
-
-    for(ll tc=1; tc<=t; tc++){
-             printf("Case %lld:\n", tc);
-             scanf("%lld %lld", &n, &m);
-             init(n);
-             while(m--){
-                      scanf("%lld %lld %lld", &u, &v, &w);
-                      graph[u].pb({v, w});
-                      graph[v].pb({u, w});
-             }
-             scanf("%lld", &src);
-             Dijkstra(src);
-
-             for(int i=0; i<n; i++){
-                      if(dis[i]!=maxx)
-                              printf("%lld\n", dis[i]);
-                      else
-                              printf("Impossible\n");
-             }
+            printf("Case %d:\n", ++cs);
+            for(int i=0; i<n; i++){
+                    if(dis[i]==INT_MAX){
+                         printf("Impossible\n");
+                         continue;
+                    }
+                    printf("%d\n", dis[i]);
+            }
     }
-
     return 0;
 }
